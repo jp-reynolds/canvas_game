32231d994dca2e7daa8f5920a18bdfc04928eacb
jp-reynolds <jpreynolds215@gmail.com>
2018-07-03 13:43:32 -0600

    OOP recreated

--- canvas.js
+++ canvas.js
@@ -24,10 +24,46 @@ var c = canvas.getContext('2d');
 // c.stroke();
 
 //ARC OR CIRCLE
-c.beginPath();
-c.arc(120, 350, 30, 0, Math.PI * 2, false);
-c.strokeStyle = 'white';
-c.stroke();
+
+function Circle(x, y, dx, dy, radius) {
+	this.x = x;
+	this.y = y;
+	this.dx = dx;
+	this.dy = dy;
+	this.radius = radius;
+
+	this.draw = function() {
+		c.beginPath();
+		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+		c.strokeStyle = 'white';
+		c.stroke();
+	};
+
+	this.update = function() {
+		if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+			this.dx = -this.dx;
+		}
+
+		if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+			this.dy = -this.dy;
+		}
+
+		this.x += this.dx;
+		this.y += this.dy;
+	};
+}
+
+var circle = new Circle(200, 200, 3, 3, 30);
+
+function animateCircle() {
+	requestAnimationFrame(animateCircle);
+	c.clearRect(0, 0, innerWidth, innerHeight);
+
+	circle.draw();
+	circle.update();
+}
+
+animateCircle();
 
 //RANDOMLY GENERATED CIRCLES
 // for (i=0; i < 5; i++) {
