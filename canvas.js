532c32101438f53068ee88b3667170b41e1e993a
jp-reynolds <jpreynolds215@gmail.com>
2018-07-03 10:45:38 -0600

    square on browser

---
+++ canvas.js
@@ -0,0 +1,14 @@
+console.log("hello");
+
+var canvas = document.querySelector('canvas')
+	;
+
+
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+
+c.fillRect(100, 100, 50, 50);
+console.log(canvas)
